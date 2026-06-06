693c05e9f5423b0072aae35ed4dbe5e2a7668533
baroren298 <baroren298@gmail.com>
2026-06-06 04:39:19 +0300

    revert

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,9 @@
-import { Suspense } from 'react';
-import { talentList } from '@/data/talent';
+import { getFeaturedTalent } from '@/data/talent';
 import { collaborations } from '@/data/collaborations';
 import HeroSection from '@/components/home/HeroSection';
 import AgencyVoice from '@/components/home/AgencyVoice';
-import TalentRoster from '@/components/talent/TalentRoster';
+import FeaturedTalent from '@/components/home/FeaturedTalent';
+import CategoryNav from '@/components/home/CategoryNav';
 import Collaborations from '@/components/home/Collaborations';
 import ContactInvite from '@/components/home/ContactInvite';
 
@@ -26,15 +26,14 @@ export const metadata = {
 };
 
 export default function HomePage() {
-  const sorted = [...talentList].sort((a, b) => a.sortOrder - b.sortOrder);
+  const featuredTalent = getFeaturedTalent(3);
 
   return (
     <>
       <HeroSection />
       <AgencyVoice />
-      <Suspense>
-        <TalentRoster talent={sorted} mode="inline" />
-      </Suspense>
+      <FeaturedTalent talent={featuredTalent} />
+      <CategoryNav />
       <Collaborations brands={collaborations} />
       <ContactInvite />
     </>
