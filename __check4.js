1e3ca214edcd7e29138e46cc0b83820fccff97cd
Bar Oren <baroren298@gmail.com>
2026-06-25 01:18:58 +0300

    Remove temporary syntax check files

--- __check4.js
+++
@@ -1,4 +0,0 @@
-const { parse } = require('@babel/parser');
-const fs = require('fs');
-parse(fs.readFileSync('components/home/ContactInvite.jsx','utf8'), { sourceType: 'module', plugins: ['jsx'] });
-console.log('OK ContactInvite.jsx');
